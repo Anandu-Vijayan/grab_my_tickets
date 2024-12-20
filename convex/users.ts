2bcb6569cb97b3f9faeb83a8e6f28dbc6171690b
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-12-20 11:26:47 +0530

    Stripe completed and create Event full done

--- convex/users.ts
+++ convex/users.ts
@@ -1,16 +1,31 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
-export const getUserById = query({
+export const getUsersStripeConnectId = query({
   args: { userId: v.string() },
-  handler: async (ctx, { userId }) => {
-    // Query the user by ID
+  handler: async (ctx, args) => {
     const user = await ctx.db
       .query("users")
-      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .filter((q) => q.neq(q.field("stripeConnectId"), undefined))
+      .first();
+    return user?.stripeConnectId;
+  },
+});
+
+export const updateOrCreateUserStripeConnectId = mutation({
+  args: { userId: v.string(), stripeConnectId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_user_id", (q) => q.eq("userId", args.userId))
       .first();
 
-    return user;
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    await ctx.db.patch(user._id, { stripeConnectId: args.stripeConnectId });
   },
 });
 
@@ -21,27 +36,41 @@ export const updateUser = mutation({
     email: v.string(),
   },
   handler: async (ctx, { userId, name, email }) => {
-    // Check if the user already exists
+    // Check if user exists
     const existingUser = await ctx.db
       .query("users")
       .withIndex("by_user_id", (q) => q.eq("userId", userId))
       .first();
 
     if (existingUser) {
-      // Update the existing user
+      // Update existing user
       await ctx.db.patch(existingUser._id, {
         name,
         email,
       });
       return existingUser._id;
     }
-    // Create a new user
+
+    // Create new user
     const newUserId = await ctx.db.insert("users", {
       userId,
       name,
       email,
       stripeConnectId: undefined,
     });
+
     return newUserId;
+  },
+});
+
+export const getUserById = query({
+  args: { userId: v.string() },
+  handler: async (ctx, { userId }) => {
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .first();
+
+    return user;
   },
 });
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        hostname:"energetic-cuttlefish-696.convex.cloud",
+        protocol:"https",
+      }
+     
+    ]
+  }
 };
 
 export default nextConfig;
