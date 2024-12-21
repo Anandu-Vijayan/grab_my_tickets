f7efc27c6128236cbb4e1b2b750b5ae75b6e036c
Anandu-Vijayan <ananduvijayan8@gmail.com>
2024-12-22 01:54:06 +0530

    Errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,16 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    remotePatterns:[
+  experimental: {
+  },
+  images: {
+    remotePatterns: [
       {
-        hostname:"energetic-cuttlefish-696.convex.cloud",
-        protocol:"https",
+        hostname: "energetic-cuttlefish-696.convex.cloud",
+        protocol: "https",
       },
-     
-      
-
-     
-    ]
-  }
+    ],
+  },
 };
 
 export default nextConfig;
